295b4a6126a13282c1ef38e61e094e748c7d3713
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2025-11-07 20:19:55 +0100

    Project Template 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "tech_app.dart";
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,11 +11,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return TechApp(
       title: "Flutter Demo",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      primary: Colors.green,
+      secondary: Colors.greenAccent,
+      themeMode: ThemeMode.system,
       home: const MyHomePage(title: "Flutter Demo Home Page"),
     );
   }
@@ -40,10 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
---
+++ lib/tech_app.dart
@@ -0,0 +1,57 @@
+import "package:flutter/material.dart";
+
+class TechApp extends MaterialApp {
+  final Color primary;
+  final Color secondary;
+  final String? fontFamily;
+  final double? fontSizeFactor;
+
+  TechApp({
+    required super.title,
+    required this.primary,
+    required this.secondary,
+    required super.themeMode,
+    required super.home,
+    super.debugShowCheckedModeBanner = false,
+    this.fontFamily,
+    this.fontSizeFactor,
+    super.routes,
+    super.key,
+  }) : super(
+         theme: ThemeData(
+           useMaterial3: false,
+           colorScheme: ColorScheme.light(primary: primary, secondary: secondary),
+         ),
+         darkTheme: ThemeData(
+           useMaterial3: false,
+           colorScheme: ColorScheme.dark(primary: primary, secondary: secondary),
+         ),
+         builder: (BuildContext context, Widget? child) {
+           return Theme(
+             data: Theme.of(context).copyWith(
+               appBarTheme: AppBarTheme(backgroundColor: primary),
+               textTheme: Theme.of(context).textTheme.apply(
+                 fontSizeFactor: fontSizeFactor ?? 1.0,
+                 fontFamily: fontFamily,
+               ),
+               scrollbarTheme: Theme.of(context).scrollbarTheme.copyWith(
+                 radius: const Radius.circular(2),
+                 thumbVisibility: const WidgetStatePropertyAll(true),
+                 trackVisibility: const WidgetStatePropertyAll(true),
+               ),
+               floatingActionButtonTheme: FloatingActionButtonThemeData(
+                 backgroundColor: primary,
+               ),
+               menuTheme: MenuThemeData(
+                 style: MenuStyle(
+                   backgroundColor: WidgetStateProperty.all(
+                     Theme.of(context).scaffoldBackgroundColor,
+                   ),
+                 ),
+               ),
+             ),
+             child: child!,
+           );
+         },
+       );
+}
